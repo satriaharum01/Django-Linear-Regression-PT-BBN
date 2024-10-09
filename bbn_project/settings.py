97d71d060608d991115d9176a4cc313217b49061
Mr. Harumi <skeith.shn@gmail.com>
2024-10-09 17:52:52 +0700

    Mysql to sqllite
    Migrate database

--- bbn_project/settings.py
+++ bbn_project/settings.py
@@ -85,14 +85,19 @@ WSGI_APPLICATION = "bbn_project.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/dev/ref/settings/#databases
 DATABASES = {
-    "default": {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'django_bbn',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST':'localhost',
-        'PORT':'3306',
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
+    #
+    #"default": {
+    #    'ENGINE': 'django.db.backends.mysql',
+    #    'NAME': 'django_bbn',
+    #    'USER': 'root',
+    #    'PASSWORD': '',
+    #    'HOST':'localhost',
+    #    'PORT':'3306',
+    #}
 }
 
 # Password validation
