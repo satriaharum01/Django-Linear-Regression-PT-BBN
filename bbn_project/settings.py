74a1467500bfd5002db59097d2ac7a49274f2d0b
Mr. Harumi <skeith.shn@gmail.com>
2024-11-06 09:32:26 +0700

    fix bug when using python 3.12

--- bbn_project/settings.py
+++ bbn_project/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -90,7 +90,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-    #
+    
     #"default": {
     #    'ENGINE': 'django.db.backends.mysql',
     #    'NAME': 'django_bbn',
@@ -136,6 +136,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/dev/howto/static-files/
 
 STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
